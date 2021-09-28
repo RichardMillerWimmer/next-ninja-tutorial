b6a3bd8218a1cfba63ddc902f06bf2a411ae2174
Richard Miller <richardmmill@gmail.com>
2021-09-28 09:47:17 -0400

    understanding next routing

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>molestie ac feugiat sed lectus vestibulum mattis ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget egestas purus viverra accumsan</p>
+            <p>pulvinar etiam non quam lacus suspendisse faucibus interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit duis tristique sollicitudin</p>
+        </div>
+    )
+}
+
+export default About
