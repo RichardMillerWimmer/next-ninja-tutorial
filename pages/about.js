7b99802efefe20de45f3c36d4b0e068600056134
Richard Miller <richardmmill@gmail.com>
2021-09-28 10:43:19 -0400

    next Image tag and meta data inside components

--- pages/about.js
+++ pages/about.js
@@ -1,13 +1,28 @@
-import React from 'react'
+import React from "react";
+import Head from 'next/head'
 
 const About = () => {
     return (
-        <div>
-            <h1>About</h1>
-            <p>molestie ac feugiat sed lectus vestibulum mattis ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget egestas purus viverra accumsan</p>
-            <p>pulvinar etiam non quam lacus suspendisse faucibus interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit duis tristique sollicitudin</p>
-        </div>
-    )
-}
+        <>
+            <Head>
+                <title>Ninja List | About</title>
+                <meta name="keywords" content="ninjas about" />
+            </Head>
+            <div>
+                <h1>About</h1>
+                <p>
+                    molestie ac feugiat sed lectus vestibulum mattis ullamcorper velit sed
+                    ullamcorper morbi tincidunt ornare massa eget egestas purus viverra
+                    accumsan
+                </p>
+                <p>
+                    pulvinar etiam non quam lacus suspendisse faucibus interdum posuere
+                    lorem ipsum dolor sit amet consectetur adipiscing elit duis tristique
+                    sollicitudin
+                </p>
+            </div>
+        </>
+    );
+};
 
-export default About
+export default About;
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,20 @@
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
+import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div>
-      <h1 className={styles.title}>Next Hompage</h1>
-      <p className={styles.text}>molestie ac feugiat sed lectus vestibulum mattis ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget egestas purus viverra accumsan</p>
-      <p className={styles.text}>pulvinar etiam non quam lacus suspendisse faucibus interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit duis tristique sollicitudin</p>
-      <Link href='/ninjas'><a className={styles.btn}>See Ninja List</a></Link>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | Home</title>
+        <meta name='keywords' content='ninjas'/>
+      </Head>
+      <div>
+        <h1 className={styles.title}>Next Hompage</h1>
+        <p className={styles.text}>molestie ac feugiat sed lectus vestibulum mattis ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget egestas purus viverra accumsan</p>
+        <p className={styles.text}>pulvinar etiam non quam lacus suspendisse faucibus interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit duis tristique sollicitudin</p>
+        <Link href='/ninjas'><a className={styles.btn}>See Ninja List</a></Link>
+      </div>
+    </>
   )
 }
