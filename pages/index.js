06df4d2e3bd273d46095d72a5598c590d1d72406
Richard Miller <richardmmill@gmail.com>
2021-09-28 10:02:33 -0400

    next routing

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <h1>Next Hompage</h1>
       <p>molestie ac feugiat sed lectus vestibulum mattis ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget egestas purus viverra accumsan</p>
       <p>pulvinar etiam non quam lacus suspendisse faucibus interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit duis tristique sollicitudin</p>
+      <Link href='/ninjas'><a>See Ninja List</a></Link>
       <Footer />
     </div>
   )
