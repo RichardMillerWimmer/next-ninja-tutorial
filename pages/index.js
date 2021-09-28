7676350ab4fdb8d6e2cc2d0e5787314619f0d30e
Richard Miller <richardmmill@gmail.com>
2021-09-28 10:18:06 -0400

    css styles and modules

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,13 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Footer from '../components/Footer'
-import Navbar from '../components/Navbar'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 
 export default function Home() {
   return (
     <div>
-      <h1>Next Hompage</h1>
-      <p>molestie ac feugiat sed lectus vestibulum mattis ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget egestas purus viverra accumsan</p>
-      <p>pulvinar etiam non quam lacus suspendisse faucibus interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit duis tristique sollicitudin</p>
-      <Link href='/ninjas'><a>See Ninja List</a></Link>
+      <h1 className={styles.title}>Next Hompage</h1>
+      <p className={styles.text}>molestie ac feugiat sed lectus vestibulum mattis ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget egestas purus viverra accumsan</p>
+      <p className={styles.text}>pulvinar etiam non quam lacus suspendisse faucibus interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit duis tristique sollicitudin</p>
+      <Link href='/ninjas'><a className={styles.btn}>See Ninja List</a></Link>
     </div>
   )
 }
